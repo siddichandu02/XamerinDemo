eb0dc8c08c81699613a8171aa040885a9d8af259
Siddi <cs185304@ncr.com>
2020-04-30 11:14:57 +0530

    updated navigation pages

---
+++ Demo/Demo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+          //  GetUser();
+        }
+        private async void OnNextPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+        
+        //private async void GetUser()
+        //{
+        //    HttpClient Client = new HttpClient();
+        //    var response = await Client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+        //    var User=JsonConvert.DeserializeObject<List<Users>>(response);
+        //    UsersListView.ItemsSource =User;
+        //}
+    }
+}
---
+++ Demo/Demo/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class Users
+    {
+        public virtual long UserId { get; set; }
+        public virtual string id { get; set; }
+        public virtual string Title { get; set; }
+        
+    }
+}
